f99644a3794fe884db581516e56c9ff340eae0fd
Nathan <100944663+x64disasm@users.noreply.github.com>
2025-11-03 16:29:41 +0800

    f

---
+++ dependencies/additions.lua
@@ -0,0 +1,132 @@
+local _getgenv = getgenv();
+local _getrenv = getrenv();
+
+local _cheatname = identifyexecutor();
+local _gettenv = clonefunction(gettenv);
+local _getreg = clonefunction(getreg);
+local _setidentity = clonefunction(setidentity);
+local _cloneref = clonefunction(cloneref);
+local _loadstring = clonefunction(loadstring);
+
+local _require = clonefunction(_getrenv.require);
+local _pcall = clonefunction(_getrenv.pcall);
+
+local _players = cloneref(game:GetService("Players"))
+local _runservice = cloneref(game:GetService("RunService"))
+local _virtualuser = cloneref(game:GetService("VirtualUser"))
+
+-- # FUCK I BROKE THIS IN C++
+_getgenv.getloadedmodules = newcclosure(function()
+    local t = {}
+    for _, v in pairs(getinstances()) do
+        if v:IsA("ModuleScript") then
+            table.insert(t, v)
+        end
+    end
+    return t
+end)
+
+_getgenv.getscriptclosure = newcclosure(function(_targetScript)
+    for _, _regEntry in pairs(_getreg()) do
+        if type(_regEntry) == "table" then
+            for _, _funcEntry in pairs(_regEntry) do
+                if type(_funcEntry) == "function" then
+                    local _env = getfenv(_funcEntry)
+                    if _env and rawget(_env, "script") == _targetScript then
+                        return _funcEntry
+                    end
+                end
+            end
+        end
+    end
+end)
+
+_getgenv.getscripthash = newcclosure(function(instance)
+    assert(typeof(instance) == "Instance", "invalid argument #1 to 'getscripthash' (Instance expected, got " .. typeof(instance) .. ")", 2)
+    assert(instance:IsA("LuaSourceContainer"), "invalid argument #1 to 'getscripthash' (LuaSourceContainer expected, got " .. instance.ClassName .. ")", 2)
+    return instance:GetHash()
+end)
+
+-- # functions below are from ballistic
+-- # credits to ballistic <3
+_getgenv.gethiddenproperty = newcclosure(function(inst, idx)
+    assert(typeof(inst) == "Instance", "invalid argument #1 to 'gethiddenproperty' [Instance expected]", 2)
+    local was = isscriptable(inst, idx)
+    setscriptable(inst, idx, true)
+
+    local val = inst[idx]
+    setscriptable(inst, idx, was)
+
+    return val, not was
+end)
+
+_getgenv.sethiddenproperty = newcclosure(function(inst, idx, val)
+    assert(typeof(inst) == "Instance", "invalid argument #1 to 'sethiddenproperty' [Instance expected]", 2)
+    local was = isscriptable(inst, idx)
+    setscriptable(inst, idx, true)
+
+    inst[idx] = val
+
+    setscriptable(inst, idx, was)
+
+    return not was
+end)
+
+_getgenv.getrunningscripts = newcclosure(function()
+    local t = {}
+    for _, v in ipairs(_players.LocalPlayer:GetDescendants()) do
+        if v:IsA("LocalScript") or v:IsA("ModuleScript") then
+            table.insert(t, v)
+        end
+    end
+    return t
+end)
+
+_getgenv.fireclickdetector = newcclosure(function(target)
+    assert(typeof(target) == "Instance", "invalid argument #1 to 'fireclickdetector' (Instance expected, got " .. type(target) .. ")", 2)
+
+    local clickDetector = target:FindFirstChild("ClickDetector") or target
+    local previousParent = clickDetector.Parent
+
+    local newPart = Instance.new("Part", workspace)
+    newPart.Transparency = 1
+    newPart.Size = Vector3.new(30, 30, 30)
+    newPart.Anchored = true
+    newPart.CanCollide = false
+
+    task.delay(15, function()
+        if newPart:IsDescendantOf(game) then
+            newPart:Destroy()
+        end
+    end)
+
+    clickDetector.Parent = newPart
+    clickDetector.MaxActivationDistance = math.huge
+
+    local heartbeatConnection
+    heartbeatConnection = _runservice.Heartbeat:Connect(function()
+        local currentCamera = workspace.CurrentCamera or workspace.Camera
+        newPart.CFrame = currentCamera.CFrame
+            * CFrame.new(0, 0, -20)
+            * CFrame.new(
+                currentCamera.CFrame.LookVector.X,
+                currentCamera.CFrame.LookVector.Y,
+                currentCamera.CFrame.LookVector.Z
+            )
+
+        _virtualuser:ClickButton1(Vector2.new(20, 20), currentCamera.CFrame)
+    end)
+
+    clickDetector.MouseClick:Once(function()
+        heartbeatConnection:Disconnect()
+        clickDetector.Parent = previousParent
+        newPart:Destroy()
+    end)
+end)
+
+_getgenv.getscripts = _getgenv.getrunningscripts
+_getgenv.fire_click_detector = _getgenv.fireclickdetector
+_getgenv.set_hidden_property = _getgenv.sethiddenproperty
+_getgenv.get_hidden_property = _getgenv.gethiddenproperty
+_getgenv.dumpstring = _getgenv.getscripthash
+_getgenv.getscriptfunction = _getgenv.getscriptclosure
